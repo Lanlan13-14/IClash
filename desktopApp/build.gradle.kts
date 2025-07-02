40e3e502cb95dc6c6365c5e648849f0236d3cad6
Lanlan13-14 <Brightandy2030999@outlook.com>
2025-07-02 22:36:04 +0800

    Create build.gradle.kts

---
+++ desktopApp/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    kotlin("jvm")
+    id("org.jetbrains.compose")
+}
+
+dependencies {
+    implementation(project(":shared"))
+    implementation(compose.desktop.currentOs)
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+    }
+}
