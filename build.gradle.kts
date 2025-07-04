909d9bf655b04a8e8845224253c9ebf6c831c3f2
Lanlan13-14 <Brightandy2030999@outlook.com>
2025-07-05 06:50:14 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,11 @@
 plugins {
-    // Android Gradle Plugin (AGP)
+    // Android 应用插件，暂时不在这里应用，只声明版本供子项目使用
     id("com.android.application") version "8.6.1" apply false
-    // Kotlin + Kotlin序列化插件
+    // Kotlin Android 插件版本
     kotlin("android") version "1.9.25" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.25" apply false
+    // Kotlin 序列化插件
+    kotlin("plugin.serialization") version "1.9.25" apply false
 }
 
-// 去掉 allprojects 的 repositories 块
-// allprojects {
-//     repositories {
-//         google()
-//         mavenCentral()
-//     }
-// }
+// 根目录的 build.gradle.kts 一般不写 repositories 和 dependencies，
+// 只负责声明插件版本和管理
