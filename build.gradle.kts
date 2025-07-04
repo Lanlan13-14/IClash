1315c1ca949d1c27c9e8259a7cf7956d983e2051
Lanlan13-14 <Brightandy2030999@outlook.com>
2025-07-04 22:43:51 +0800

    更新 build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,4 @@
 plugins {
-    // Android Gradle Plugin
-    id("com.android.application") version "8.5.0" apply false
-
-    // Kotlin Android 插件，升级到与 Compose Compiler 兼容的版本
-    kotlin("android") version "1.9.25" apply false
+    id("com.android.application") version "8.6.1" apply false
+    kotlin("android")               version "1.9.25" apply false
 }
