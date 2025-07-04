1bb900471715686e05022cfc6c9008376c5ea1b8
Lanlan13-14 <Brightandy2030999@outlook.com>
2025-07-05 06:37:11 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,14 @@
 plugins {
+    // 与 Android 15 (API 35) 正式版配套的 AGP
     id("com.android.application") version "8.6.1" apply false
-    kotlin("android")               version "1.9.25" apply false
+    // Kotlin 1.9.25 + 序列化编译插件
+    kotlin("android")                     version "1.9.25" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.25" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
