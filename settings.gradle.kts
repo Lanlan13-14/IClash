6dc3a548d089a05e58d6d968ac6538ef7a82b2a8
Lanlan13-14 <Brightandy2030999@outlook.com>
2025-07-05 06:43:53 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,7 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
+    // 避免在子项目声明仓库，强制所有仓库由这里统一管理
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
